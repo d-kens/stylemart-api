0dec833fbaa363545ecbb1085848c1476d99e161
d-kens <omondikens1@gmail.com>
2024-11-06 06:30:19 +0300

    project setup with swagger documentation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATASOURCE_HOST,
+      port: parseInt(process.env.DATASOURCE_PORT, 10) || 3306,
+      username: process.env.DATASOURCE_USERNAME,
+      password: process.env.DATASOURCE_PASSWORD,
+      database: process.env.DATASOURCE_DATABASE,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    AuthModule, 
+    UsersModule
+  ],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }
+  ));
+
+  const config = new DocumentBuilder()
+    .setTitle('Stylemart API')
+    .setDescription('The Stylemart API allows users to interact with an online fashion store, offering features like browsing products, managing orders, and user authentication.')
+    .setVersion('1.0')
+    .addTag('Products')
+    .addTag('Orders') 
+    .addTag('Users')
+    .addTag('Payments')
+    .addTag('Cart')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().then(() => {
+  Logger.log(`🚀 Application is running on: http://localhost:${process.env.PORT}`)
+});
