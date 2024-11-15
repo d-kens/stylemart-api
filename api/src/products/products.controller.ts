aa40daf0293d3aa19fd354b9dbf38bd3bc3a4cf1
d-kens <omondikens1@gmail.com>
2024-11-15 15:08:09 +0300

    product crud operations

--- api/src/products/products.controller.ts
+++ api/src/products/products.controller.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Body, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { response, Response } from 'express';
+import { Response } from 'express';
 import { CreateProductDto } from './dto/create-product.dto';
+import { ProductResponseDto } from './dto/product-response.dto';
 
 
 @Controller('products')
@@ -11,6 +12,18 @@ export class ProductsController {
     constructor(
         private productsService: ProductsService
     ){}
+
+    @Get()
+    async getAllProducts(): Promise<ProductResponseDto[]> {
+        const products = await this.productsService.getAllProducts();
+        return products.map(product => new ProductResponseDto(product));
+    }
+
+    @Get(':id')
+    async getProductById(@Param('id') id: string): Promise<ProductResponseDto> {
+        const product = await this.productsService.getProductById(id);
+        return new ProductResponseDto(product);
+    }
 
     @Post()
     @UseInterceptors(FileInterceptor('file', {
@@ -34,9 +47,14 @@ export class ProductsController {
             callback(null, true)
         }
     }))
-    async createProduct(@UploadedFile() file: Express.Multer.File, @Body() productData: CreateProductDto) {
-        console.log(file)
-        const result = await this.productsService.createProduct(productData, file)
+    async createProduct(@UploadedFile() file: Express.Multer.File, @Body() productData: CreateProductDto): Promise<ProductResponseDto> {
+        const result = await this.productsService.createProduct(productData, file);
+        return new ProductResponseDto(result);
+    }
+
+    @Delete(':id')
+    async deleteProduct(@Param('id') id: string): Promise<void> {
+        await this.productsService.deleteProduct(id);
     }
 
     @Get('image/:filename')
