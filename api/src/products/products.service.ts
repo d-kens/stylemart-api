61a487abf5c6680d0020ab36c9346ffa555255f1
d-kens <omondikens1@gmail.com>
2024-11-15 15:08:09 +0300

    product crud operations

--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { unlink } from 'fs';
 import { join } from 'path';
+import * as process from 'process';
 
 @Injectable()
 export class ProductsService {
@@ -15,40 +16,73 @@ export class ProductsService {
         private categoryService: CategoriesService,
     ) {}
 
-
-    private async unlinkFile(filePath: string): Promise<void> {
-        
+    async getAllProducts(): Promise<Product[]> {
+        return this.productsRepository.find();
     }
 
+    async getProductById(productId: string): Promise<Product> {
+        return await this.productsRepository.findOne({
+            where: { id: productId }
+        });
+    }
 
-    async createProduct(productData: CreateProductDto, image: Express.Multer.File) {
+    async createProduct(productData: CreateProductDto, image: Express.Multer.File): Promise<Product> {
         const category = await this.categoryService.findCategoryById(productData.categoryId);
 
-        console.log(category)
+        productData.imageUrl = `${process.env.BASE_URL}/products/image/${image.filename}`
 
         if (!category) {
-
-            const filePath = join(process.cwd(), 'uploads/products/', image.filename);
-
-            console.log(filePath)
-
-            console.log('Invalid Category. Unlinking File');
-
-            unlink(filePath, (err) => {
-                if (err) {
-                    console.error(err);
-                }
-            });
+            await this.unlinkFile(productData.imageUrl)
             throw new NotFoundException(`Category with id: ${productData.categoryId} not found`);
         }
 
-        const product = new Product({
-            ...productData,
-            price: Number(productData.price),
-            stock: Number(productData.stock),
-        });
+        try {
 
-        return product;
+            const product = new Product({
+                ...productData,
+                price: Number(productData.price),
+                stock: Number(productData.stock),
+            });
+    
+            return await this.productsRepository.save(product);
+        } catch (error) {
+            console.log(error)
+            throw new InternalServerErrorException('Failed to create product')
+        }
+    }
+
+    /**
+     * TODO: Implement product update operation
+     */
+
+    async deleteProduct(productId: string): Promise<void> {
+
+        const product = await this.getProductById(productId);
+        
+        if (!product) {
+            throw new NotFoundException(`Product with id: ${productId} not found`);
+        }
+
+        try {
+            await this.unlinkFile(product.imageUrl)
+
+            await this.productsRepository.delete(product);
+
+        } catch (error) {
+            throw new InternalServerErrorException('Failed to delete product')
+        }
+    }
+
+    private async unlinkFile(imageUrl: string): Promise<void> {
+        const filename = imageUrl.split('/').pop();
+
+        const filePath = join(process.cwd(), 'uploads/products/', filename);
+
+        unlink(filePath, (err) => {
+            if (err) {
+               throw err;
+            }
+        });
     }
 
 }
