8dc702b66aa5b4c3dbbf7baa459794452b0363f6
d-kens <omondikens1@gmail.com>
2024-11-19 14:57:21 +0300

    get and initializes users cart

--- api/src/carts/carts.controller.ts
+++ api/src/carts/carts.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CartsService } from './carts.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/entities/user.entity';
+import { CartItemDto } from './dto/cart-item.dto';
 
 @Controller('carts')
-export class CartsController {}
+export class CartsController {
+    constructor(
+        private cartsService: CartsService,
+    ) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getCart(@CurrentUser() user: User) {
+        return await this.cartsService.getCart(user.id)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('add-item')
+    async addItemToCart(@CurrentUser() user: User, @Body() cartItemData: CartItemDto) {
+        return this.cartsService.addItemToCart(user.id, cartItemData);
+    }
+}
--- api/src/carts/carts.service.ts
+++ api/src/carts/carts.service.ts
@@ -1,4 +1,106 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartItem } from 'src/entities/cart-item.entity';
+import { Cart } from 'src/entities/cart.entity';
+import { Repository } from 'typeorm';
+import { CartItemDto } from './dto/cart-item.dto';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class CartsService {}
+export class CartsService {
+    constructor(
+        @InjectRepository(Cart)
+        private cartsRepository: Repository<Cart>,
+        @InjectRepository(CartItem)
+        private cartItemsRepository: Repository<CartItem>,
+        private readonly dataSource: DataSource,
+    ) {}
+
+    async getCart(userId: string): Promise<Cart> {
+
+        let cart = await this.cartsRepository.findOne({
+            where: { userId },
+            relations: ['cartItems', 'cartItems.product'],
+        })
+
+        if(!cart) {
+            cart = this.cartsRepository.create({userId, total: 0, cartItems: []});
+            cart = await this.cartsRepository.save(cart);
+        }
+
+        return cart;
+    }
+
+    async addItemToCart(userId: string, cartItemData: CartItemDto): Promise<boolean> {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+
+        try {
+            const { productId, quantity } = cartItemData
+
+            let cart = await this.getCart(userId);
+
+            let cartItem = await this.cartItemsRepository.findOne({
+                where: {cartId: cart.id, productId}
+            });
+            
+            if (cartItem) {
+                cartItem.quantity += quantity;
+            } else {
+                cartItem = this.cartItemsRepository.create({
+                    cartId: cart.id,
+                    productId,
+                    quantity
+                });
+            }
+
+            await queryRunner.manager.save(cartItem);
+
+            const cartItems = await queryRunner.manager.find(CartItem, {
+                where: { cartId: cart.id },
+                relations: ['product']
+            })
+
+            cart.total = cartItems.reduce(
+                (sum, item) => sum + item.quantity * item.product.price,
+                0
+            );
+
+            await queryRunner.manager.save(cart);
+
+
+            await queryRunner.commitTransaction();
+
+            return true
+        } catch (err) {
+            await queryRunner.rollbackTransaction();
+            console.error(err)
+            throw new InternalServerErrorException('Failed to add item to cart');
+        } finally {
+            await queryRunner.release();
+        }
+    }
+
+
+
+    private async updateCartTotal(cart: Cart): Promise<Cart> {
+        const items = await this.cartItemsRepository.find({
+            where: { cartId: cart.id },
+            relations: ['product']
+        })
+
+        console.log(items)
+
+        let total = 0;
+
+        items.forEach(item => {
+            total += item.product.price * item.quantity;
+        });
+
+        cart.total = total;
+        return await this.cartsRepository.save(cart);
+    }
+
+ }
