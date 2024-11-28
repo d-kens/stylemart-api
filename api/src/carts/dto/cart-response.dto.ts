5e362604210e6842389b90cc4190e9be38a5b9a8
d-kens <omondikens1@gmail.com>
2024-11-28 19:09:32 +0300

    Updated CartResponseDto to include full nested structure in Swagger

--- api/src/carts/dto/cart-response.dto.ts
+++ api/src/carts/dto/cart-response.dto.ts
@@ -1,27 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-
-/**
- * TODO: Having the entire thing on the response 
- */
-
-export class CartResponseDto {
-    @ApiProperty()
-    id: string;
-
-    @ApiProperty()
-    total: number;
-
-    @ApiProperty()
-    cartItems: CartItemResponseDto[];
-
-    constructor(cart: any) {
-        this.id = cart.id;
-        this.total = cart.total;
-        this.cartItems = cart.cartItems.map(item => new CartItemResponseDto(item));
-    }
-}
-
 export class CartItemResponseDto {
     @ApiProperty()
     id: string;
@@ -30,19 +8,37 @@ export class CartItemResponseDto {
     quantity: number;
 
     @ApiProperty()
-    productId: string; 
+    productId: string;
 
     @ApiProperty()
-    productQuantity: number; 
+    productQuantity: number;
 
     @ApiProperty()
-    imageUrl: string; 
+    imageUrl: string;
 
     constructor(item: any) {
         this.id = item.id;
         this.quantity = item.quantity;
-        this.productId = item.product.id; 
-        this.productQuantity = item.product.stock; 
-        this.imageUrl = item.product.imageUrl; 
+        this.productId = item.product.id;
+        this.productQuantity = item.product.stock;
+        this.imageUrl = item.product.imageUrl;
+    }
+}
+
+
+export class CartResponseDto {
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty()
+    total: number;
+
+    @ApiProperty({ type: [CartItemResponseDto] })
+    cartItems: CartItemResponseDto[];
+
+    constructor(cart: any) {
+        this.id = cart.id;
+        this.total = cart.total;
+        this.cartItems = cart.cartItems.map(item => new CartItemResponseDto(item));
     }
 }
