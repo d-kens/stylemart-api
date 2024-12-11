d7457eb2b1045071cdb9f7ff17eb86e7a7f20ab4
d-kens <omondikens1@gmail.com>
2024-12-11 07:27:19 +0300

    user entity definition

---
+++ auth/src/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import { RoleEnum } from "src/enums/role.enum";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity('tbl_users')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ name: 'firstname', type: 'varchar', length: 60 })
+    firstName: string;
+
+    @Column({ name: 'lastname', type: 'varchar', length: 100 })
+    lastName: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
+    email: string;
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    password: string;
+
+    @Column({ name: 'phone_number', type: 'varchar', length: 20, nullable: false })
+    phoneNumber: string;
+
+    @Column({ type: 'boolean', default: false })
+    isEmailVerified: boolean;
+
+    @Column({
+        type: 'enum',
+        enum: RoleEnum,
+        default: RoleEnum.USER
+    })
+    role: RoleEnum;
+
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp'})
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: "updated_at", type: 'timestamp' })
+    updatedAt: Date;
+}
+
