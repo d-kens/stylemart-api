89b911b66c06046d37714cf899399ad8faebc53e
d-kens <omondikens1@gmail.com>
2024-12-19 08:38:28 +0300

    setup the frontend with the required dependencies

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome()
+  }
+
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  
 }
---
+++ web/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser
+];
