52ab4f608bbf6ea69b3e165ead6ad690fc9a3666
d-kens <omondikens1@gmail.com>
2024-12-24 09:42:02 +0300

    implement all authentiocation related screens: login, reguster, forgot pwd, reset pwd and email verification component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { EmailVerificationComponent } from './auth/email-verification/email-verification.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
     {
@@ -9,11 +11,19 @@ export const routes: Routes = [
         component: SignUpComponent
     },
     {
-        path: 'auht/email-verification',
-        component: EmailVerificationComponent
-    },
-    {
         path: 'auth/sign-in',
         component: SignInComponent
+    },
+    {
+        path: 'auth/forgot-password',
+        component: ForgotPasswordComponent,
+    },
+    {
+        path: 'auth/reset-password',
+        component: ResetPasswordComponent,
+    },
+    {
+        path: 'auth/verify-email',
+        component: EmailVerificationComponent,
     }
 ];
