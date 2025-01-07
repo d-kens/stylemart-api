ae005b33a846c719d6383b52a94b91b0965d6be8
d-kens <omondikens1@gmail.com>
2025-01-07 12:57:16 +0300

    Test mpesa stk callback

--- src/payment-gateway/payment-gateway.module.ts
+++ src/payment-gateway/payment-gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PaymentGatewayService } from "./payment-gateway.service";
 import { PaymentGatewayController } from "./payment-gateway.controller";
 import { MpesaModule } from "./payment-providers/mpesa/mpesa.module";
+import { TransactionsModule } from "./transactions/transactions.module";
 
 @Module({
-  imports: [MpesaModule],
+  imports: [MpesaModule, TransactionsModule],
   providers: [PaymentGatewayService],
   controllers: [PaymentGatewayController],
 })
